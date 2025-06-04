90ef8a79afcdf5cd145a94768a08c4831a86e870
shillaa12 <tasyanoorazhila12@students.unnes.ac.id>
2025-06-04 20:50:48 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Resto Semarang", layout="wide")
+
+st.title("📊 Dashboard Klastering Restoran di Semarang")
+st.markdown("""
+Selamat datang di dashboard interaktif untuk eksplorasi dan analisis klaster restoran di Semarang.  
+
+Navigasi:
+- **Background**: Latar belakang dan tujuan
+- **Data Visualization**: EDA dan insight awal
+- **Hasil Analisis**: Hasil model Analisis
+- **Prediction Form**: Formulir prediksi untuk data baru
+""")
